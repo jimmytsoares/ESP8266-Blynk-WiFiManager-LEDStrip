01345851452d8acbc02830b29e5e116020bf4885
Jimmy <jimmytsoares@gmail.com>
2017-10-10 01:01:38 -0300

    Add deepsleep para esquentar menos quando não estiver usando

--- BlynkWifiManager_LEDStrip/BlynkWiFiManager_LEDStrip.ino
+++ BlynkWifiManager_LEDStrip/BlynkWiFiManager_LEDStrip.ino
@@ -1,5 +1,3 @@
-// Código usando o WiFiManager feito originalmente pelo proprio criador tzapu e aprimorado por Costas do
-// fórum do Blynk
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 //#define BLYNK_DEBUG           // Comment this out to disable debug and save space
 //#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
@@ -7,61 +5,38 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
-#include <Adafruit_NeoPixel.h>  // para facilitar os comandos pra fita de LED
-//for LED status
-#include <Ticker.h>
-Ticker ticker;
-
+#include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 
 char blynk_token[34] = "BLYNK_TOKEN";
 
-int arcoiro = 0;
-
-int timer_id;
-
-int R = 0;
+int arcoiro = 0; //variavel seletora de efeito
+int timer_id; //id do timer para rodar os efeitos
+int R = 0; 
 int G = 0;
 int B = 0;
-
-int wait_time = 10;
+int deepSleep = 0; //define se entra ou não em deepsleep
+int wait_time = 10; //delay dos efeitos
 
 bool shouldSaveConfig = false; //flag for saving data
 
-
 #include <BlynkSimpleEsp8266.h>
 #include <SimpleTimer.h>
-SimpleTimer timer;
 BlynkTimer timer_blynk;
 
 #define NUMPIXELS 23
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, D9, NEO_RGB + NEO_KHZ800);
 
-void tick()
-{
-  //toggle state
-  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
-  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
-}
-
 void saveConfigCallback () {  //callback notifying us of the need to save config
   Serial.println("Should save config");
   shouldSaveConfig = true;
-  ticker.attach(0.2, tick);  // led toggle faster
 }
 
 void setup()
 {
-
+/////////////////////////START of TZAPU WifiManager Code///////////////////////////
   Serial.begin(115200);
   Serial.println();
-
-  //set led pin as output
-  pinMode(BUILTIN_LED, OUTPUT);
-  // start ticker with 0.5 because we start in AP mode and try to connect
-  ticker.attach(0.6, tick);
-
-  //SPIFFS.format();    //clean FS, for testing
   Serial.println("Mounting FS...");    //read configuration from FS json
 
   if (SPIFFS.begin()) {
@@ -82,9 +57,7 @@ void setup()
         json.printTo(Serial);
         if (json.success()) {
           Serial.println("\nparsed json");
-
           strcpy(blynk_token, json["blynk_token"]);
-
         } else {
           Serial.println("Failed to load json config");
         }
@@ -93,7 +66,6 @@ void setup()
   } else {
     Serial.println("Failed to mount FS");
   }
-  //end read
 
   // The extra parameters to be configured (can be either global or just in the setup)
   // After connecting, parameter.getValue() will get you the configured value
@@ -107,36 +79,21 @@ void setup()
   WiFiManager wifiManager;
 
   wifiManager.setSaveConfigCallback(saveConfigCallback);   //set config save notify callback
-
-  //set static ip
-  // this is for connecting to Office router not GargoyleTest but it can be changed in AP mode at 192.168.4.1
-  //wifiManager.setSTAStaticIPConfig(IPAddress(192,168,10,111), IPAddress(192,168,10,90), IPAddress(255,255,255,0));
   
   wifiManager.addParameter(&custom_blynk_token);   //add all your parameters here
 
-  //wifiManager.resetSettings();  //reset settings - for testing
-
-  //set minimu quality of signal so it ignores AP's under that quality
-  //defaults to 8%
-  //wifiManager.setMinimumSignalQuality();
-  
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep, in seconds
   wifiManager.setTimeout(600);   // 10 minutes to enter data and then Wemos resets to try again.
 
   //fetches ssid and pass and tries to connect, if it does not connect it starts an access point with the specified name
   //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect("CentralHeatingAP", "MY123PWD")) {
+  if (!wifiManager.autoConnect("jimts33", "12345678")) {
     Serial.println("Failed to connect and hit timeout");
     delay(3000);
     //reset and try again, or maybe put it to deep sleep
     ESP.reset();
     delay(5000);
   }
-  Serial.println("Connected Central Heating System :)");   //if you get here you have connected to the WiFi
-  ticker.detach();
-  //turn LED off
-  digitalWrite(BUILTIN_LED, HIGH);
+  Serial.println("Connected to WiFi");   //if you get here you have connected to the WiFi
 
   strcpy(blynk_token, custom_blynk_token.getValue());    //read updated parameters
 
@@ -159,18 +116,18 @@ void setup()
 
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
+/////////////////////////END of TZAPU WifiManager Code///////////////////////////
   
   Blynk.config(blynk_token);
   Blynk.connect();
-  //timer.setInterval(10000L, runArcoIro);
   timer_id = timer_blynk.setInterval(1000L, effectsTimer);
   timer_blynk.disable(timer_id);
   strip.begin();
   strip.show();
   
-
 }
 
+//efeitos precisam rodar varias vezes, então o timer faz isso pra gente
 void effectsTimer()
 {
   switch (arcoiro)
@@ -180,11 +137,13 @@ void effectsTimer()
     case 3: snakeBounce(wait_time); break;
     case 4: snakeBounceHalf(wait_time);
   }
+
 }
 
+//Seletor de efeito
 BLYNK_WRITE(V1)
 {
-  arcoiro = param.asInt(); // assigning incoming value from pin V1 to a variable
+  arcoiro = param.asInt();
   if(arcoiro >= 1)
   {
     timer_blynk.enable(timer_id);
@@ -203,6 +162,20 @@ BLYNK_WRITE(V1)
   }
 }
 
+//Toda vez que resetar, irá rodar cada Blynk_write
+BLYNK_CONNECTED() {
+    Blynk.syncAll();
+}
+
+//Ativa o deepsleep por 20s, deixando o módulo menos quente, mas desabilita efeitos
+BLYNK_WRITE(V0)
+{
+  deepSleep = param.asInt();
+  if(deepSleep == 1)
+  ESP.deepSleep(20000000);
+}
+
+//Pega do widget zeRGBra, valores de 0-255 e muda a cor da fita
 BLYNK_WRITE(V2)
 {
   R = param[0].asInt();
@@ -216,6 +189,7 @@ BLYNK_WRITE(V2)
 
 }
 
+//Muda o tempo de delay dos efeitos
 BLYNK_WRITE(V3)
 {
   wait_time = param.asInt();
@@ -226,9 +200,11 @@ BLYNK_WRITE(V3)
   void loop()
 {
   Blynk.run(); // Initiates Blynk
-  timer.run(); // Initiates SimpleTimer  
+  //timer.run(); // Initiates SimpleTimer  
   timer_blynk.run();
 }
+
+//////////////////ADAFRUIT NEOPIXEL FUNCTIONS//////////////////
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
